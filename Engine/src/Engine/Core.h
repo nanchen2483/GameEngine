a1c25ba30036158078e2af0d2a03ff54dc40328f
Nan Chen <nanx2483@gmail.com>
2021-03-21 17:11:19 +0100

    Setup project

---
+++ Engine/src/Engine/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifdef ENGINE_PLATFORM_WINDOWS
+	#ifdef ENGINE_BUILD_DLL	
+		#define ENGINE_API __declspec(dllexport)
+	#else
+		#define ENGINE_API __declspec(dllimport)
+	#endif // ENGINE_BUILD_DLL
+#else
+	#error Engine only support windows
+#endif // ENGINE_PLATFORM_WINDOWS
