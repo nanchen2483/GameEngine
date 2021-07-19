912c9530c65a717aaf61592e92e215ae1f0e3041
Nan Chen <nanx2483@gmail.com>
2021-07-19 23:40:45 +0200

    refact(premake): update premake file

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,6 @@
+include "./util/premake/premake_customization/solution_items.lua"
+include "dependencies.lua"
+
 workspace "GameEngine"
 	architecture "x64"
 	startproject "xEditor"
@@ -9,207 +12,26 @@ workspace "GameEngine"
 		"Dist"
 	}
 
+	solution_items
+	{
+		".editorconfig"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
--- Include directory relative to root folder (solution directory)
-IncludeDir = {}
-IncludeDir["glfw"] = "Engine/util/glfw/include"
-IncludeDir["glad"] = "Engine/util/glad/include"
-IncludeDir["glm"] = "Engine/util/glm"
-IncludeDir["imgui"] = "Engine/util/imgui"
-IncludeDir["stb_image"] = "Engine/util/stb_image"
-IncludeDir["entt"] = "Engine/util/entt/include"
-IncludeDir["yaml_cpp"] = "Engine/util/yaml-cpp/include"
-IncludeDir["ImGuizmo"] = "Engine/util/ImGuizmo"
-
 group "Dependencies"
+	include "util/premake"
 	include "Engine/util/glfw"
 	include "Engine/util/glad"
 	include "Engine/util/imgui"
 	include "Engine/util/yaml-cpp"
 group ""
 
-project "Engine"
-	location "Engine"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("imd/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "enginepch.h"
-	pchsource "Engine/src/enginepch.cpp"
-	
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/util/stb_image/**",
-		"%{prj.name}/util/ImGuizmo/ImGuizmo.h",
-		"%{prj.name}/util/ImGuizmo/ImGuizmo.cpp",
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/util/spdlog/include",
-		"%{IncludeDir.glfw}",
-		"%{IncludeDir.glad}",
-		"%{IncludeDir.imgui}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.entt}",
-		"%{IncludeDir.yaml_cpp}",
-		"%{IncludeDir.ImGuizmo}"
-	}
-
-	links
-	{
-		"GLFW",
-		"glad",
-		"imgui",
-		"opengl32.lib",
-		"yaml-cpp"
-	}
-
-	filter "files:Engine/util/ImGuizmo/**.cpp" 
-		flags { "NoPCH" }
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"ENGINE_PLATFORM_WINDOWS",
-			"ENGINE_BUILD_DLL",
-			"GLFW_INCLUDE_NONE"
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		runtime "Release"
-		symbols "on"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		runtime "Release"
-		symbols "on"
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	cppdialect "C++17"
-	language "C++"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("imd/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-	}
-
-	includedirs
-	{
-		"Engine/util/spdlog/include",
-		"Engine/util/imgui",
-		"Engine/src",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}",
-	}
-
-	links
-	{
-		"Engine"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"ENGINE_PLATFORM_WINDOWS",
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		runtime "Release"
-		symbols "on"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		runtime "Release"
-		symbols "on"
-
-project "xEditor"
-	location "xEditor"
-	kind "ConsoleApp"
-	cppdialect "C++17"
-	language "C++"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("imd/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-	}
-
-	includedirs
-	{
-		"Engine/util/spdlog/include",
-		"Engine/util/imgui",
-		"Engine/src",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}",
-		"%{IncludeDir.ImGuizmo}"
-	}
-
-	links
-	{
-		"Engine"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"ENGINE_PLATFORM_WINDOWS",
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		runtime "Release"
-		symbols "on"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		runtime "Release"
-		symbols "on"
+include "Engine"
+include "Sandbox"
+include "xEditor"
