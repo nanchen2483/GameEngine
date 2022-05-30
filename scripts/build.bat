542ac854b531738743859e8bce7131bb9d1dfe16
Nan Chen <nanx2483@gmail.com>
2022-05-30 17:12:05 +0200

    fix(batch): add validation to build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,8 @@
 @echo off
 where ninja
-if %ERRORLEVEL% neq 0 (
-	echo 'Ninja' not found 
-) else (
-	cmake -S %~dp0..\ --preset=x64-debug
-)
+if %ERRORLEVEL% neq 0 (echo 'Ninja' not found exit 1)
+
+where cl.exe
+if %ERRORLEVEL% neq 0 (echo 'cl.exe' not found exit 1)
+
+call cmake -S %~dp0..\ --preset=x64-debug
--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -1,5 +1,5 @@
 @echo off
-pushd ..\
+pushd %~dp0..\
 call git clean -dfx -e .vs
 popd
 if /i not "%~1"=="nopause" pause
