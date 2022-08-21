6b0f5043e0fdeb25b2929297b154cc9d4ae9e59e
Nan Chen <nanx2483@gmail.com>
2022-08-21 19:11:43 +0200

    feat(math): add Math::Sign method

--- Engine/src/Engine/Math/Math.h
+++ Engine/src/Engine/Math/Math.h
@@ -6,5 +6,11 @@ namespace Engine
 	namespace Math
 	{
 		bool DecomposeTransform(const glm::mat4 transform, glm::vec3& outTranslation, glm::vec3& outRotation, glm::vec3& outScale);
+
+		template <typename T>
+		int Sign(T value)
+		{
+			return (T(0) < value) - (value < T(0));
+		}
 	}
 }
