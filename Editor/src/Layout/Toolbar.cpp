79f9f9f8c9c3ee519d31165f66e5889d0b66aa55
Nan Chen <nanx2483@gmail.com>
2022-11-19 21:14:03 +0100

    refactor: rewirte Toolbar

--- Editor/src/Layout/Toolbar.cpp
+++ Editor/src/Layout/Toolbar.cpp
@@ -14,36 +14,50 @@ namespace Engine
 
 	void Toolbar::OnImGuiRender()
 	{
-		static float thumbnailSize = 32.0f;
+		m_thumbnailSize = 32.0f;
 		if (ImGui::Begin("Toolbar", 0, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse))
 		{
 			float panelWidth = ImGui::GetContentRegionAvail().x;
-			ImGui::SetCursorPosX((panelWidth - thumbnailSize) * 0.5f);
+			ImGui::SetCursorPosX((panelWidth - m_thumbnailSize) * 0.5f);
 			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
-			if (m_play)
-			{
-				ImGui::ImageButton((ImTextureID)(uint64_t)m_pauseIcon->GetRendererId(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
-				if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
-				{
-					m_pause = true;
-				}
-				ImGui::SameLine();
-				ImGui::ImageButton((ImTextureID)(uint64_t)m_stopIcon->GetRendererId(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
-				if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
-				{
-					m_play = false;
-				}
-			}
-			else
-			{
-				ImGui::ImageButton((ImTextureID)(uint64_t)m_playIcon->GetRendererId(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
-				if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+			ImageButton(m_playIcon, m_play, [=]()
 				{
 					m_play = true;
-				}
-			}
+				});
+			ImGui::SameLine();
+			ImageButton(m_pauseIcon, !m_play, [=]()
+				{
+					m_pause = true;
+				});
+			ImGui::SameLine();
+			ImageButton(m_stopIcon, !m_play, [=]()
+				{
+					m_play = false;
+				});
 			ImGui::PopStyleColor();
 		}
 		ImGui::End();
+	}
+	
+	void Toolbar::ImageButton(Ptr<Texture2D> texture, bool isDisabled, std::function<void(void)> OnClick)
+	{
+		if (isDisabled)
+		{
+			ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
+		}
+		
+		ImGui::ImageButton((ImTextureID)(uint64_t)texture->GetRendererId(), { m_thumbnailSize, m_thumbnailSize }, { 0, 1 }, { 1, 0 });
+		
+		if (isDisabled)
+		{
+			ImGui::PopItemFlag();
+			ImGui::PopStyleVar();
+		}
+
+		if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+		{
+			OnClick();
+		}
 	}
 }
