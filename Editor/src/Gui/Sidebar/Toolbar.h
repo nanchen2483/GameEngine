5a7ddf9010fcd2904a23fddde7ee7fd8eb0c20ad
Nan Chen <nanx2483@gmail.com>
2022-12-04 02:18:29 +0100

    feat(toolbar): add saveAs and openFolder button to toolbar

--- Editor/src/Gui/Sidebar/Toolbar.h
+++ Editor/src/Gui/Sidebar/Toolbar.h
@@ -2,6 +2,8 @@
 #include "Gui/IGui.h"
 #include "Engine.h"
 
+#include <functional>
+
 namespace Engine
 {
 	class Toolbar : public IGui
@@ -11,8 +13,11 @@ namespace Engine
 
 		virtual void OnImGuiRender() override;
 		bool IsPlaying() const { return m_play; }
+
+		typedef std::function<void()> Func;
+		void SetFunctions(Func newScene, Func openScene, Func saveSceneAs);
 	private:
-		void ImageButton(Ptr<Texture2D> texture, bool isDisabled, std::function<void(void)> OnClick);
+		void ImageButton(Ptr<Texture2D> texture, bool isDisabled, Func OnClick);
 
 		bool m_play = false;
 		bool m_pause = false;
@@ -22,8 +27,15 @@ namespace Engine
 		const glm::vec2 m_toolbarPadding = { 7.0f, 7.0f };
 		const float m_toolbarHeight = m_buttonSizeWithPadding.y + (m_toolbarPadding.y * 2);
 		
+		Ptr<Texture2D> m_openIcon;
+		Ptr<Texture2D> m_saveIcon;
+
 		Ptr<Texture2D> m_playIcon;
 		Ptr<Texture2D> m_pauseIcon;
 		Ptr<Texture2D> m_stopIcon;
+
+		std::function<void()> NewScene;
+		std::function<void()> OpenScene;
+		std::function<void()> SaveSceneAs;
 	};
 }
