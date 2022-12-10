710ef32bd0a15cd93cfd51dedb9f5f288296eea1
Nan Chen <nanx2483@gmail.com>
2022-12-10 20:25:47 +0100

    feat(deltaTime): add GetDeltaTime to system

--- Engine/src/Engine/Core/System/System.h
+++ Engine/src/Engine/Core/System/System.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Engine/Core/Window/Input.h"
 
+#include <chrono>
 #include <glm/glm.hpp>
 
 namespace Engine
@@ -8,12 +9,16 @@ namespace Engine
 	class System
 	{
 	public:
+		static double GetDeltaTime() { return s_deltaTime; }
 		static glm::vec2 GetMouseDeltaPosition() { return s_deltaMousePosition; }
 
 		static void OnUpdate();
 	private:
+		static void OnTimeUpdate();
 		static void OnMouseUpdate();
 
+		static std::chrono::system_clock::time_point s_time;
+		static double s_deltaTime;
 		static glm::vec2 s_mousePosition;
 		static glm::vec2 s_deltaMousePosition;
 		static float s_mouseVelocity;
