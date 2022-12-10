aa5449efe0c2a618270464fb37ea4f8cc86c65f7
Nan Chen <nanx2483@gmail.com>
2022-12-10 16:00:23 +0100

    refactor(component): remove logic from component

--- Engine/src/Engine/Scene/Component.h
+++ Engine/src/Engine/Scene/Component.h
@@ -31,7 +31,6 @@ namespace Engine
 		glm::vec3 GetTranslation() const { return transform.translation; }
 		glm::vec3 GetRotation() const { return transform.rotation; }
 		glm::vec3 GetScale() const { return transform.scale; }
-		glm::mat4 GetViewMatrix() const { return glm::inverse((glm::mat4)transform); }
 		
 		operator glm::mat4() const { return transform; }
 		operator Transform&() { return transform; }
