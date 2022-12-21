e1b789c0033ebc9b9e07bc2642c1f8a3eb95a998
Nan Chen <nanx2483@gmail.com>
2022-12-21 21:23:57 +0100

    chore(log): remove debug and critical options from log

--- Engine/src/Engine/Core/Log/Log.h
+++ Engine/src/Engine/Core/Log/Log.h
@@ -23,11 +23,9 @@ namespace Engine
 }
 
 #define ENGINE_CORE_TRACE(...)		::Engine::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define ENGINE_CORE_DEBUG(...)		::Engine::Log::GetCoreLogger()->debug(__VA_ARGS__)
 #define ENGINE_CORE_INFO(...)		::Engine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define ENGINE_CORE_WARN(...)		::Engine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define ENGINE_CORE_ERROR(...)		::Engine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define ENGINE_CORE_CRITICAL(...)	::Engine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 #define ENGINE_TRACE(...)			::Engine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define ENGINE_INFO(...)			::Engine::Log::GetClientLogger()->info(__VA_ARGS__)
