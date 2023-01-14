df6c4826219826510be6eb4bd801cf63d424873c
Nan Chen <nanx2483@gmail.com>
2023-01-14 22:20:10 +0100

    fix(model): remove unused code from AssimpAnimation class

--- Engine/src/Engine/Util/Assimp/AssimpAnimation.cpp
+++ Engine/src/Engine/Util/Assimp/AssimpAnimation.cpp
@@ -18,30 +18,4 @@ namespace Engine
 			m_boneTransforms.push_back(glm::mat4(1.0f));
 		}
 	}
-
-	void AssimpAnimation::UpdateBoneTransforms()
-	{
-		CalculateAnimationTime();
-		CalculateBoneTransform(m_rootNode, glm::mat4(1.0f));
-	}
-
-	void AssimpAnimation::CalculateAnimationTime()
-	{
-		*m_animationTime += m_ticksPerSecond * System::GetDeltaTime();
-		*m_animationTime = fmod(*m_animationTime, m_duration);
-	}
-
-	void AssimpAnimation::CalculateBoneTransform(const Ptr<Node> node, glm::mat4 globalTransformation)
-	{
-		globalTransformation *= node->GetTransform(*m_animationTime);
-		if (node->AnyBones())
-		{
-			m_boneTransforms[node->GetBoneId()] = globalTransformation * node->GetBoneOffset();
-		}
-
-		for (uint32_t i = 0; i < node->GetNumOfChildNodes(); i++)
-		{
-			CalculateBoneTransform(node->GetChildNode(i), globalTransformation);
-		}
-	}
 }
--- Engine/src/Engine/Util/Assimp/AssimpAnimation.h
+++ Engine/src/Engine/Util/Assimp/AssimpAnimation.h
@@ -16,12 +16,7 @@ namespace Engine
 		virtual const Ptr<Node> GetRootNode() const { return m_rootNode; }
 		virtual std::vector<glm::mat4> GetBoneTransforms() const override { return m_boneTransforms; };
 		virtual void UpdateBoneTransforms(uint32_t boneId, glm::mat4 updatedTransform) override { m_boneTransforms[boneId] = updatedTransform; };
-
-		void UpdateBoneTransforms();
 	private:
-		void CalculateAnimationTime();
-		void CalculateBoneTransform(const Ptr<Node> node, glm::mat4 globalTransformation);
-		
 		const std::string m_name;
 		Ptr<float> m_animationTime;
 		const float m_duration;
