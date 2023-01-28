d65c07a1c3a7984e84055f2c93f3c5e99dc9e860
Nan Chen <nanx2483@gmail.com>
2023-01-28 13:01:53 +0100

    fix(buffer): clear vector data after use

--- Engine/src/Engine/Util/Assimp/AssimpMesh.cpp
+++ Engine/src/Engine/Util/Assimp/AssimpMesh.cpp
@@ -17,8 +17,8 @@ namespace Engine
 			m_vertexArray = VertexArray::Create();
 			m_vertexArray->AddVertexBuffer(VertexBuffer::Create(&(m_vertices)[0], m_vertices.size()));
 			m_vertexArray->SetIndexBuffer(IndexBuffer::Create(&(m_indices)[0], m_indices.size()));
-			m_vertices = {};
-			m_indices = {};
+			std::vector<Vertex>().swap(m_vertices);
+			std::vector<uint32_t>().swap(m_indices);
 		}
 
 		return m_vertexArray;
