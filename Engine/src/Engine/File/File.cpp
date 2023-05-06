e7365799e5e28b6babb543d84d2fc89853a58ec3
Nan Chen <nanx2483@gmail.com>
2023-05-06 15:16:40 +0200

    refactor(extension): update FileExtensions

--- Engine/src/Engine/File/File.cpp
+++ Engine/src/Engine/File/File.cpp
@@ -5,7 +5,6 @@
 #include "FileType.h"
 
 #include <algorithm>
-#include <set>
 
 namespace Engine
 {
@@ -13,19 +12,19 @@ namespace Engine
 	{
 		std::string extension = file.extension().string();
 
-		std::set<std::string> scene{ FileExtension::SceneFile::Engine };
-		if (std::find(scene.begin(), scene.end(), extension) != scene.end())
+		std::set<std::string> scenes = FileExtension::SceneExtension::All;
+		if (std::find(scenes.begin(), scenes.end(), extension) != scenes.end())
 		{
 			return FileType::Scene;
 		}
 
-		std::set<std::string> textures{ FileExtension::TextureFile::Png, FileExtension::TextureFile::Jpg, FileExtension::TextureFile::Bmp };
+		std::set<std::string> textures = FileExtension::TextureExtension::All;
 		if (std::find(textures.begin(), textures.end(), extension) != textures.end())
 		{
 			return FileType::Texture;
 		}
 
-		std::set<std::string> models{ FileExtension::ModelFile::Dae, FileExtension::ModelFile::Obj };
+		std::set<std::string> models = FileExtension::ModelExtension::All;
 		if (std::find(models.begin(), models.end(), extension) != models.end())
 		{
 			return FileType::Model;
--- Engine/src/Engine/File/FileExtension.h
+++ Engine/src/Engine/File/FileExtension.h
@@ -1,26 +1,44 @@
 #pragma once
 #include <string>
+#include <set>
 
 namespace Engine
 {
 	namespace FileExtension
 	{
-		namespace SceneFile
+		namespace SceneExtension
 		{
 			const std::string Engine = ".engine";
+			const std::set<std::string> All
+			{
+				Engine
+			};
 		};
 
-		namespace TextureFile
+		namespace TextureExtension
 		{
 			const std::string Png = ".png";
 			const std::string Jpg = ".jpg";
 			const std::string Bmp = ".bmp";
+			const std::set<std::string> All
+			{
+				Png,
+				Jpg,
+				Bmp
+			};
 		};
 
-		namespace ModelFile
+		namespace ModelExtension
 		{
 			const std::string Dae = ".dae";
 			const std::string Obj = ".obj";
+			const std::string Blend = ".blend";
+			const std::set<std::string> All
+			{
+				Dae,
+				Obj,
+				Blend
+			};
 		};
 	}
 }
