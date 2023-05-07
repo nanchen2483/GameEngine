bac83cfd591277124f15e97badc698011eba648d
Nan Chen <nanx2483@gmail.com>
2023-05-07 02:57:23 +0200

    fix(script): clean all untracked files from vcpkg

--- Scripts/cleanup.bat
+++ Scripts/cleanup.bat
@@ -1,8 +1,27 @@
 @echo off
 
-rem Clean all untracked file except .vs
+set cleanAll=0
+if "%1" == "--all" (
+    set cleanAll=1
+) else if "%1" == "-a" (
+    set cleanAll=1
+) else if "%1" == "" (
+    rem Ignore
+) else if "%1" == " " (
+    rem Ignore
+) else (
+    echo unknown option: "%1"
+    exit 1
+)
+
+rem Clean all untracked files except .vs
 pushd %~dp0..
 call git clean -dfx -e .vs
+if %cleanAll% == 1 (
+    pushd Dependencies\vcpkg
+    call git clean -dfx
+    popd
+)
 popd
 
 if /i not "%~1"=="nopause" pause
